d3802d3c950730909effb3e3935d77fd2ce367f9
sekcayala <aayala39@mail.csuchico.edu>
2018-02-07 18:15:59 -0800

    thissucked

--- main.cpp
+++ main.cpp
@@ -1,24 +1,32 @@
-//Author:
+//Author:Alejandro Ayala
 #include<iostream>
 
 using namespace std;
 
 int main()
 {
-
+char letter;
   cout<<"What character do you want to know about?\n";
+  cin>> letter;
 
   //when user's entry is between A-Z...
-  cout<<" is an upper case letter!\n";
-
+  if (letter>='A' && letter<='Z')
+  {
+  cout<< letter<<" is an upper case letter!\n";
+  }
   //when user's entry is between a-z...
-  cout<<" is a lower case letter!\n";
-
+  else if (letter>='a' && letter<='z')
+  {
+  cout<< letter<<" is a lower case letter!\n";
+  }
   //in all other cases...
-  cout<<"?! Pssh. What are you talking about?\n";
-  
+  else if (letter>='A' && letter<='z')
+  {
+  cout<< letter<<"?! Pssh. What are you talking about?\n";
+  }
   //no matter what they enter...
   cout<<"The ASCII value is: ";
+  cout<<(int) letter<<endl;
 
   return 0;
 }
